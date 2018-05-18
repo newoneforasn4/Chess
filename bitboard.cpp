670618a714b086bdaf81cbadbff26956733cbabe
Rank <yangbeny@sfu.ca>
2018-05-18 13:12:53 -0700

    source update

--- bitboard.cpp
+++ bitboard.cpp
@@ -3,7 +3,12 @@
 // Initializer
 bitboard::bitboard()
 {
-	
+	resetBoard();
+}
+
+bitboard::bitboard(u64 bboard[])
+{
+	setBoard(bboard);
 }
 
 bitboard::~bitboard()
@@ -12,10 +17,21 @@ bitboard::~bitboard()
 
 u64 bitboard::getboard()
 {
-	return board;
+	u64 bboard = 0;
+	for (int i = 0; i < 12; i++)
+		bboard^=board[i];
+	return bboard;
+}
+
+void bitboard::setBoard(u64 bboard[])
+{
+	for (int i = 0; i < 12; i++)
+		board[i] = bboard[i]
+	return;
 }
 
 void bitboard::resetBoard()
 {
 	board = {wP,wN,wB,wR,wQ,wK,bP,bN,bB,bR,bQ,bK};
+	return;
 }
